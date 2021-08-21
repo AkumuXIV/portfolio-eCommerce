23f8e8c2348fc4768b69ca18643b235d85185e9c
Jay <jdavis627@gmail.com>
2021-08-20 19:20:15 -0700

    added cart icon, cart dropdown, and redux reducers for cart.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, hidden}) => {
     return (
         <div className='header'>
             <Link to='/' className='logo-container'>
@@ -27,14 +29,23 @@ const Header = ({currentUser}) => {
                     (<Link className='option' to='/signin'>SIGN IN</Link>)
 
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ? 
+                null
+                :
+                <CartDropdown />
+            }
+            
         </div>
     );
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => {
     return {
-        currentUser: state.user.currentUser
+        currentUser,
+        hidden
     };
 };
 
